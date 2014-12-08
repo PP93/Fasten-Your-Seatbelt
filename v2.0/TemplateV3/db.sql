6933a2068f52a4f56b69267bc3b4fb3bb58d6cf3
Floris van Lent <florisvanlent@hotmail.com>
2014-12-08 02:16:31 +0100

    Employee Class to make our lives easier
    We need a better database first. This is far from perfect and changing
    database means LOADS of changes EVERYWHERE ELSE D:

--- v2.0/TemplateV3/db.sql
+++ v2.0/TemplateV3/db.sql
@@ -1,46 +1,46 @@
-CREATE TABLE `employee` (
-`employeeID` INT(3) NOT NULL AUTO_INCREMENT primary key,
-`name` CHAR(20) NOT NULL ,
-`lastname` CHAR(20) NOT NULL,
-`username` CHAR(20) NOT NULL,
-`password` CHAR(20) NOT NULL,
-`email` CHAR(50) NOT NULL,
-`phonenumber` CHAR(30) NOT NULL,
-`function` CHAR(50) NOT NULL
-);
-
-CREATE TABLE `client` (
-`clientID` INT(3) NOT NULL AUTO_INCREMENT primary key,
-`name` CHAR(20) NOT NULL ,
-`lastname` CHAR(20) NOT NULL ,
-`phonenumber` CHAR(30) NOT NULL ,
-`email` CHAR(50) NOT NULL ,
-`country` CHAR(20) NOT NULL ,
-`address` CHAR(50) NOT NULL ,
-`city` CHAR(50) NOT NULL ,
-`zipcode` CHAR(20) NOT NULL ,
-`shippingcountry` CHAR(50) NULL ,
-`shippingaddress` CHAR(50) NULL ,
-`shippingcity` CHAR(50) NULL ,
-`shippingzipcode` CHAR(50) NULL 
-);
-
-CREATE TABLE `baggage` (
-`baggageID` INT(3) NOT NULL AUTO_INCREMENT primary key,
-`flightnumber` CHAR(20) NOT NULL ,
-`brand` CHAR(20) NULL ,
-`color` CHAR(30) NULL ,
-`weight` CHAR(50) NULL ,
-`description` VARCHAR(200) NULL
-);
-
-CREATE TABLE `log` (
-`logID` INT(6) NOT NULL AUTO_INCREMENT primary key,
-`employeeID` INT(3) NOT NULL,
-`timestamp` TIMESTAMP(6),
-`action` CHAR(255) NOT NULL,
-`tab` CHAR(255) NOT NULL
-);
+-- CREATE TABLE `employee` (
+-- `employeeID` INT(3) NOT NULL AUTO_INCREMENT primary key,
+-- `name` CHAR(20) NOT NULL ,
+-- `lastname` CHAR(20) NOT NULL,
+-- `username` CHAR(20) NOT NULL,
+-- `password` CHAR(20) NOT NULL,
+-- `email` CHAR(50) NOT NULL,
+-- `phonenumber` CHAR(30) NOT NULL,
+-- `function` CHAR(50) NOT NULL
+-- );
+-- 
+-- CREATE TABLE `client` (
+-- `clientID` INT(3) NOT NULL AUTO_INCREMENT primary key,
+-- `name` CHAR(20) NOT NULL ,
+-- `lastname` CHAR(20) NOT NULL ,
+-- `phonenumber` CHAR(30) NOT NULL ,
+-- `email` CHAR(50) NOT NULL ,
+-- `country` CHAR(20) NOT NULL ,
+-- `address` CHAR(50) NOT NULL ,
+-- `city` CHAR(50) NOT NULL ,
+-- `zipcode` CHAR(20) NOT NULL ,
+-- `shippingcountry` CHAR(50) NULL ,
+-- `shippingaddress` CHAR(50) NULL ,
+-- `shippingcity` CHAR(50) NULL ,
+-- `shippingzipcode` CHAR(50) NULL 
+-- );
+-- 
+-- CREATE TABLE `baggage` (
+-- `baggageID` INT(3) NOT NULL AUTO_INCREMENT primary key,
+-- `flightnumber` CHAR(20) NOT NULL ,
+-- `brand` CHAR(20) NULL ,
+-- `color` CHAR(30) NULL ,
+-- `weight` CHAR(50) NULL ,
+-- `description` VARCHAR(200) NULL
+-- );
+-- 
+-- CREATE TABLE `log` (
+-- `logID` INT(6) NOT NULL AUTO_INCREMENT primary key,
+-- `employeeID` INT(3) NOT NULL,
+-- `timestamp` TIMESTAMP(6),
+-- `action` CHAR(255) NOT NULL,
+-- `tab` CHAR(255) NOT NULL
+-- );
 
 INSERT INTO employee
 (name, lastname, username, password, email, phonenumber, function)
---
+++ v2.0/TemplateV3/src/fys/Employee.java
@@ -0,0 +1,54 @@
+package fys;
+
+import java.awt.HeadlessException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ * It works for now. But we need a perfect database first
+ * 
+ * @author Floris
+ */
+public class Employee {
+    public int employeeID;
+    public String firstName;
+    public String lastName;
+    public String username;
+    public String password;
+    public String emailAddress;
+    public String phoneNumber;
+    public String function;
+
+    public Employee(int employeeID) {
+        Connection conn = javaconnect.ConnecrDb();
+        try {
+            String sql = "select * from employee where employeeID=?";
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setString(1, "" + employeeID);
+            ResultSet rs = pst.executeQuery();
+
+            if (rs.next()) {
+                this.employeeID  = employeeID;
+                this.firstName   = rs.getString("name");
+                this.lastName    = rs.getString("lastname");
+                this.username    = rs.getString("username");
+                this.password    = rs.getString("password");
+                this.phoneNumber = rs.getString("phonenumber");
+                this.function    = rs.getString("function");
+
+            } else {
+                System.out.println("THIS WENT WRONG");
+            }
+
+        } catch (SQLException | HeadlessException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
+    
+    public String toString() {
+        return employeeID + " " + firstName + " " + lastName + " " + username + " " + function;
+    }
+}
